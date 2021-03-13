f53ed94be09a33b901a5355c1548230200bcca79
riadhhamoudi <riadhhamoudi11@gmail.com>
2021-03-13 14:35:58 +0100

    first commit

---
+++ myapp/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Profile from './profile/Profile';
+
+const App=()=>{
+  return (
+    <Profile fullname='riadh' bio='test' profession='test' />
+  )
+}
+
+export default App;
---
+++ myapp/src/profile/Profile.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Profile = (props) => {
+    console.log(pros);
+    return (
+        <div>
+        <p>{props.fullname}</p>
+        <p>{props.bio}</p>
+        <p>{props.profession}</p>
+
+            
+        </div>
+    )
+}
+
+export default Profile
